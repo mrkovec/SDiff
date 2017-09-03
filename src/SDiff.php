05743a3162d018fff3c7eaf34746704e03e4e656
mrkovec <mrkovec@gmail.com>
2017-09-03 12:17:29 +0200

    code refactor

--- src/SDiff.php
+++ src/SDiff.php
@@ -6,39 +6,43 @@ namespace mrkovec\sdiff;
 class SDiff {
 
   // compute diff for arbytrary object
-  public static function getObjectDiff($a, $b, $retEqual = False)
+  public static function getObjectDiff($a, $b, $retEqual = True)
   {
+    ksort($a);
+    ksort($b);
     $a = json_encode($a, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
-    $b = json_encode($b, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT); //JSON_UNESCAPED_UNICODE|
-    // return mb_convert_encoding(self::getTextDiff($a, $b, $spliter, $retEqual), "UTF-8");
-    return self::getClauseDiff($a, $b, False);
+    $b = json_encode($b, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+    $diff = self::getClauseDiff($a, $b, $retEqual);
+    if($diff["eq"] === 1) return null;
+    return $diff["diff"];
   }
 
   public static function getClauseDiff($a, $b, $retEqual = True)
   {
-    return self::getDiff($a, $b, [["spliter" => "\n", "retEqual" => $retEqual],["spliter" => " ", "retEqual" => $retEqual],["spliter" => "", "retEqual" => True]])["diff"];
+    return self::getDiff($a, $b, [["separator" => "\n", "retEqual" => $retEqual],["separator" => " "],["separator" => ""]]);
   }
 
   public static function getWordDiff($a, $b, $retEqual = True)
   {
-    return self::getDiff($a, $b, [["spliter" => " ", "retEqual" => $retEqual],["spliter" => ""]])["diff"];
+    return self::getDiff($a, $b, [["separator" => " ", "retEqual" => $retEqual],["separator" => ""]]);
   }
 
   public static function getCharDiff($a, $b, $retEqual = True)
   {
-    return self::getDiff($a, $b, [["spliter" => "", "retEqual" => $retEqual]])["diff"];
+    return self::getDiff($a, $b, [["separator" => "", "retEqual" => $retEqual]]);
   }
 
-  public static function getDiff($a, $b, $params = [["spliter" => "", "retEqual" => True]])
+  public static function getDiff($a, $b, $params = [["separator" => "", "retEqual" => True]])
   {
     if (!is_string($a) || !is_string($a)) throw new \Exception('not a string');
-    if ($a === $b) return ["eq" => 1, "diff" => $a];
 
-    if (count($params) === 0) return ["eq" => 0, "diff" => null];
+    if ($a === $b) {
+      return ["eq" => 1, "diff" => $a];
+    }
 
-    $spliter = "";
-    if(isset($params[0]["spliter"])) {
-      $spliter = $params[0]["spliter"];
+    $separator = "";
+    if(isset($params[0]["separator"])) {
+      $separator = $params[0]["separator"];
     }
 
     $retEqual = True;
@@ -46,11 +50,14 @@ class SDiff {
       $retEqual = $params[0]["retEqual"];
     }
 
+
+    if (count($params) === 0) return ["eq" => 0, "diff" => null];
+
     array_splice($params, 0, 1);
 
-    if($spliter) {
-      $text1 = explode($spliter, $a);
-      $text2 = explode($spliter, $b);
+    if($separator) {
+      $text1 = explode($separator, $a);
+      $text2 = explode($separator, $b);
     } else {
       $text1 = preg_split('//u', $a, -1, PREG_SPLIT_NO_EMPTY);
       $text2 = preg_split('//u', $b, -1, PREG_SPLIT_NO_EMPTY);
@@ -87,14 +94,14 @@ class SDiff {
           array_splice($text2, 0, $o["i"]+1);
         }
 
-        if(strpos($o["val"], "<del>") !== False || strpos($o["val"], "<ins>") !== False) $res[] = $o["val"];
-        elseif($retEqual) $res[] = $o["val"];
+        if($o["eq"] < 1) $res[] = $o["val"];
+        elseif ($retEqual) $res[] = $o["val"];
 
         $numEqual += $o["eq"];
       }
       array_splice($text1, 0, 1);
     }
-    return ["eq" => $numEqual/$numChar, "diff" => implode($res,$spliter)];
+    return ["eq" => $numEqual/$numChar, "diff" => implode($res,$separator)];
   }
 
 
@@ -135,39 +142,20 @@ class SDiff {
     return ["i" => $i, "val" => $diffval, "eq" => $diff["eq"]];
   }
 
-
-
-
-  public static function formHtml($str)
-  {
-    return self::formatJson($str, True);
-  }
-  /**
-   * Formats a JSON string for pretty printing
-   *
-   * @param string $json The JSON to make pretty
-   * @param bool $html Insert nonbreaking spaces and <br />s for tabs and linebreaks
-   * @return string The prettified output
-   * @author Jay Roberts
-   */
-  private static function formatJson($json, $html = false) {
+   public static function formatHtml($json) {
       $tabcount = 0;
       $result = '';
       $inquote = false;
-      $ignorenext = false;
-      if ($html) {
-          $tab = "&nbsp;&nbsp;&nbsp;";
-          $newline = "<br/>";
-      } else {
-          $tab = "\t";
-          $newline = "\n";
-      }
+      // $ignorenext = false;
+      $tab = "&nbsp;&nbsp;";
+      $newline = "<br/>";
+
       for($i = 0; $i < strlen($json); $i++) {
           $char = $json[$i];
-          if ($ignorenext) {
-              $result .= $char;
-              $ignorenext = false;
-          } else {
+          // if ($ignorenext) {
+          //     $result .= $char;
+          //     $ignorenext = false;
+          // } else {
               switch($char) {
                   case '{':
                       $tabcount++;
@@ -178,20 +166,21 @@ class SDiff {
                       $result = trim($result) . $newline . str_repeat($tab, $tabcount) . $char;
                       break;
                   case ',':
-                      $result .= $char . $newline . str_repeat($tab, $tabcount);
+                      if ($inquote) $result .= $char;
+                      else $result .= $char . $newline . str_repeat($tab, $tabcount);
                       break;
                   case '"':
                       $inquote = !$inquote;
                       $result .= $char;
                       break;
-                  case '\\':
-                      if ($inquote) $ignorenext = true;
-                      $result .= $char;
-                      break;
+                  // case '\\':
+                  //     if ($inquote) $ignorenext = true;
+                  //     $result .= $char;
+                  //     break;
                   default:
                       $result .= $char;
               }
-          }
+          // }
       }
       return $result;
     }
